1e45bab0ae4d6ba33c6f637183e6e3e5fbfac244
gustavo koutian <gustavokoutian@gmail.com>
2017-10-25 18:14:40 -0300

    resolucion de problema

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ const pass = "pass";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.set('port', (process.env.PORT || 4000));
 
 mongoose.connect(`mongodb://${user}:${pass}@ds231205.mlab.com:31205/price-history`, (err, res) => {
     if (err) {
         console.log(chalk.red.bold("No se pudo conectar a la base de datos"))
     } else {
         console.log(chalk.green.bold("Conexion a la base de datos realizada"))
-        app.listen(4000);
-        console.log(chalk.blue.bold(`Servidor express iniciado en el puerto 4000`));
+        app.listen(app.get('port'));
+        console.log(chalk.blue.bold('Servidor express iniciado en el puerto ', app.get('port')));
     }
 })
 
